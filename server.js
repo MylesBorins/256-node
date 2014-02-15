556eb6752d6080802209f124b5d4c30188decd18
Myles Borins <myles.borins@gmail.com>
2014-02-15 03:58:12 -0800

    Really good start

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ var server = http.createServer(function (req, res) {
   'use strict';
   
   if (req.method === 'POST' && req.url === '/soundshare/sound') {
-    console.log('Post Request');
-    consumer.addFile(req);
-    res.end();
+    consumer.addFile(req)
+    .then(function () {
+      res.end();
+    });
   }
   else if (req.method === 'GET' && req.url === '/soundshare/sounds') {
-    console.log('Get Request');
     consumer.getFiles()
     .then(function (files) {
-      console.log(files);
       res.writeHead(200, {'Content-Type': 'application/json'});
       res.end(JSON.stringify(files));
     });
